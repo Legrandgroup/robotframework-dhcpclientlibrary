7bb91592fbaf019b0e2890f0253170b47c542e7d
Lionel Ains <lionel.ains@legrand.fr>
2014-06-27 10:02:07 +0200

    Cleanup and using real if MAC address in packets

--- BasicDhcpClient.py
+++ BasicDhcpClient.py
@@ -5,13 +5,16 @@ from __future__ import print_function
 
 import sys
 
-sys.path.insert(0, '/mnt/exported/dydhcplib/lib/python2.7/site-packages/')
+sys.path.insert(0, '/opt/pydhcplib2/lib/python2.7/site-packages/')
 
 import dbus
 import dbus.service
 import dbus.mainloop.glib
 
 from random import Random
+
+import MacAddr
+
 r = Random()
 r.seed()
 
@@ -64,7 +67,7 @@ def dhcpTypeToName(type, exception_on_unknown = True):
 
 
 class BasicDhcpClient(DhcpClient, dbus.service.Object):
-	def __init__(self, conn, object_path=DBUS_OBJECT_PATH, ifname = None, listen_address = '0.0.0.0', client_port = 68, server_port = 67, **kwargs):
+	def __init__(self, conn, object_path=DBUS_OBJECT_PATH, ifname = None, listen_address = '0.0.0.0', client_port = 68, server_port = 67, mac_addr = None, **kwargs):
 		"""
 		Instanciate a new BasicDhcpClient client bound to ifname (if specified) or a specific interface address listen_address (if specified)
 		Client listening UDP port and server destination UDP port can also be overridden from their default values
@@ -83,6 +86,17 @@ class BasicDhcpClient(DhcpClient, dbus.service.Object):
 		self._listen_address = listen_address
 		self._client_port = client_port
 		self._server_port = server_port
+		self._last_ipaddress = None
+		self._last_netmask = None
+		self._last_defaultgw = None
+		self._request_sent = False
+		if mac_addr == None:
+			if self._ifname:
+				self._mac_addr = MacAddr.getHwAddrForIf(ifname = self._ifname)
+			elif self._listen_address != '0.0.0.' and self._listen_address != '::':
+				self._mac_addr = MacAddr.getHwAddrForIp(ip = self._listen_address)
+			else:
+				raise Exception('NoInterfaceProvided')
 		self.genNewXid()
 	
 	# D-Bus-related methods
@@ -134,54 +148,61 @@ class BasicDhcpClient(DhcpClient, dbus.service.Object):
 		"""
 		Send a DHCP DISCOVER packet to the network
 		"""
-		req = DhcpPacket()
-		req.SetOption('op', [1])
-		req.SetOption('htype', [1])
-		req.SetOption('hlen', [6])
-		req.SetOption('hops', [0])
-		req.SetOption('xid', self._getXitAsDhcpOption())
-		req.SetOption('giaddr',ipv4('0.0.0.0').list())
-		req.SetOption('chaddr',hwmac('00:12:34:56:78:9a').list() + [0] * 10)
-		req.SetOption('ciaddr',ipv4('0.0.0.0').list())
-		req.SetOption('siaddr',ipv4('0.0.0.0').list())
-		req.SetOption('dhcp_message_type',[dhcpNameToType('DISCOVER')])
-		#	req.SetOption('parameter_request_list',1)
+		dhcp_discover = DhcpPacket()
+		dhcp_discover.SetOption('op', [1])
+		dhcp_discover.SetOption('htype', [1])
+		dhcp_discover.SetOption('hlen', [6])
+		dhcp_discover.SetOption('hops', [0])
+		dhcp_discover.SetOption('xid', self._getXitAsDhcpOption())
+		dhcp_discover.SetOption('giaddr',ipv4('0.0.0.0').list())
+		dhcp_discover.SetOption('chaddr',hwmac(self._mac_addr).list() + [0] * 10)
+		dhcp_discover.SetOption('ciaddr',ipv4('0.0.0.0').list())
+		dhcp_discover.SetOption('siaddr',ipv4('0.0.0.0').list())
+		dhcp_discover.SetOption('dhcp_message_type',[dhcpNameToType('DISCOVER')])
+		#	dhcp_discover.SetOption('parameter_request_list',1)
 		#client.dhcp_socket.settimeout(timeout)
-		req.SetOption('flags',[128, 0])
-		req_type = req.GetOption('dhcp_message_type')[0]
-		print("Sending " + dhcpTypeToName(req_type))
-		self.SendDhcpPacketTo(req, "255.255.255.255", self._server_port)
+		dhcp_discover.SetOption('flags',[128, 0])
+		dhcp_discover_type = dhcp_discover.GetOption('dhcp_message_type')[0]
+		print("Sending " + dhcpTypeToName(dhcp_discover_type))
+		self._request_sent = False
+		self.SendDhcpPacketTo(dhcp_discover, '255.255.255.255', self._server_port)
 	
 	def handleDhcpOffer(self, res):
 		"""
 		Handle a DHCP OFFER packet coming from the network
 		"""
-		dhcp_message_type = res.GetOption('dhcp_message_type')[0]
+		dhcp_offer = res
+		dhcp_message_type = dhcp_offer.GetOption('dhcp_message_type')[0]
 		print("Received " + dhcpTypeToName(dhcp_message_type, False))
-		print(res.str())
-		server_identifier = ipv4(res.GetOption('server_identifier'))
-		chaddr = hwmac(res.GetOption('chaddr')[:6])
-		yiaddr = ipv4(res.GetOption('yiaddr'))
-		request_ciaddr = yiaddr.str()
-		serverip = server_identifier.str()
-		request_dhcp_message_type = 'request'
-		req2 = DhcpPacket()
-		req2.SetOption('op',[1])
-		req2.SetOption('htype',[1])
-		req2.SetOption('hlen',[6])
-		req2.SetOption('hops',[0])
-		req2.SetOption('xid', self._getXitAsDhcpOption())
-		req2.SetOption('giaddr',ipv4('0.0.0.0').list())
-		req2.SetOption('chaddr',hwmac('00:12:34:56:78:9a').list() + [0] * 10)
-		req2.SetOption('ciaddr',yiaddr.list())
-		req2.SetOption('siaddr',server_identifier.list())
-		req2.SetOption('dhcp_message_type',[dhcpNameToType('REQUEST')])
-		#	req2.SetOption('parameter_request_list',1)
+		print(dhcp_offer.str())
+		server_identifier = ipv4(dhcp_offer.GetOption('server_identifier'))
+		chaddr = hwmac(dhcp_offer.GetOption('chaddr')[:6])
+		yiaddr = ipv4(dhcp_offer.GetOption('yiaddr'))
+		self._last_ipaddress = ipv4(dhcp_offer.GetOption('yiaddr'))
+		print("We've been given IP: " + str(dhcp_offer.GetOption('yiaddr')))
+		self._last_netmask = ipv4(dhcp_offer.GetOption('subnet_mask'))
+		print("We've been given netmask: " + str(dhcp_offer.GetOption('subnet_mask')))
+		self._last_defaultgw =  ipv4(dhcp_offer.GetOption('router'))
+		print("We've been given gw: " + str(dhcp_offer.GetOption('router')))# Router is of type ipv4+ so we could get more than one router IPv4 address... only consider the first one here
+		
+		dhcp_request = DhcpPacket()
+		dhcp_request.SetOption('op',[1])
+		dhcp_request.SetOption('htype',[1])
+		dhcp_request.SetOption('hlen',[6])
+		dhcp_request.SetOption('hops',[0])
+		dhcp_request.SetOption('xid', self._getXitAsDhcpOption())
+		dhcp_request.SetOption('giaddr',ipv4('0.0.0.0').list())
+		dhcp_request.SetOption('chaddr',hwmac(self._mac_addr).list() + [0] * 10)
+		dhcp_request.SetOption('ciaddr',yiaddr.list())
+		dhcp_request.SetOption('siaddr',server_identifier.list())
+		dhcp_request.SetOption('dhcp_message_type',[dhcpNameToType('REQUEST')])
+		#	dhcp_request.SetOption('parameter_request_list',1)
 		#self.dhcp_socket.settimeout(timeout)
-		req2.SetOption('flags',[128, 0])
-		req2_type = req2.GetOption('dhcp_message_type')[0]
-		print("Sending " + dhcpTypeToName(req2_type))
-		self.SendDhcpPacketTo(req2, serverip, self._server_port)
+		dhcp_request.SetOption('flags',[128, 0])
+		dhcp_request_type = dhcp_request.GetOption('dhcp_message_type')[0]
+		print("Sending " + dhcpTypeToName(dhcp_request_type))
+		self._request_sent = True
+		self.SendDhcpPacketTo(dhcp_request, '255.255.255.255', self._server_port)
 	
 	def HandleDhcpOffer(self, res):
 		"""
@@ -191,6 +212,8 @@ class BasicDhcpClient(DhcpClient, dbus.service.Object):
 	
 	def handleDhcpAck(self, packet):
 		print("Received ACK")
+		if self._request_sent:
+			print("DHCP server acknowledged our IP")
 		print(packet.str())
 		
 	def HandleDhcpAck(self, packet):
